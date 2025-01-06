beee9940e623e84b8fe8fb3c8d9807deb0a50e07
huyennk6 <huyennk6@fpt.edu.vn>
2025-01-06 10:51:31 +0700

    buoi 1: bien & kieu du lieu

---
+++ MOB1014_SD20301/src/mob1014_sd20301/MOB1014_SD20301.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mob1014_sd20301;
+
+/**
+ *
+ * @author Huyen
+ */
+public class MOB1014_SD20301 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //gõ nhanh main
+    //main + tab
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Hello world");
+        //click chuột phải chọn -> Run File
+    }
+    /*
+    2 cách kiểm tra JDK
+    1: windown R -> cmd -> nhập javac -version
+    2: vào control panel -> program -> program & features 
+    => sau khi cài đặt JDK -> cài đặt Netbean
+    */
+    
+}
